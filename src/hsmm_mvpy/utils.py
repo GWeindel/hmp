LOOCV plot and result storage was in the wrong order

src/hsmm_mvpy/utils.py
@@ -155,7 +155,6 @@ def read_mne_data(pfiles, event_id=None, resp_id=None, epoched=False, sfreq=None
                 data, events = data.resample(sfreq, events=events)
             combined =  {**event_id, **resp_id}#event_id | resp_id 
             stim = list(event_id.keys())
-            print(stim)
             
             if verbose:
                 print(f'Creating epochs based on following event ID :{np.unique(events[:,2])}')
@@ -723,7 +722,7 @@ def loocv_mp(init, stacked_data, bests, func=LOOCV, cpus=2, verbose=True):
                     itertools.repeat(bests.sel(n_events=n_events)), itertools.repeat(init.sfreq)))
         loocv.append(loo)
 
-    loocv = xr.DataArray(np.array(loocv)[:,:,0].astype(np.float64), coords={"n_event":np.arange(1,bests.n_events.max().values+1),
+    loocv = xr.DataArray(np.array(loocv)[:,:,0].astype(np.float64), coords={"n_event":np.arange(1,bests.n_events.max().values+1)[::-1],
                                                            "participants":np.array(loocv)[0,:,1]}, name="loo_likelihood")
     return loocv
 

src/hsmm_mvpy/visu.py
@@ -180,10 +180,10 @@ def plot_loocv(loocv_estimates, pvals=True, test='t-test', figsize=(16,5), indiv
         alpha=1
     if indiv:
         for loo in loocv_estimates.T:
-            ax[0].plot(np.arange(loocv_estimates.n_event.max())+1,loo, alpha=alpha)
+            ax[0].plot(loocv_estimates.n_event,loo, alpha=alpha)
     ax[0].set_ylabel('LOOCV Loglikelihood')
     ax[0].set_xlabel('Number of events')
-    ax[0].set_xticks(ticks=np.arange(1,loocv_estimates.n_event.max()+1))
+    ax[0].set_xticks(ticks=loocv_estimates.n_event)
     total_sub = len(loocv_estimates.participants)
     diffs, diff_bin, labels = [],[],[]
     for n_event in np.arange(2,loocv_estimates.n_event.max()+1):