a5d6ab4fe5f0ebe403059b47d333dcc77b035151
GWeindel <gabriel.weindel@gmail.com>
2023-09-24 08:27:10 +0200

    Trying by adjusting to real time (aka between samples) leaving last one as is

--- src/hsmm_mvpy/models.py
+++ src/hsmm_mvpy/models.py
@@ -1124,9 +1124,8 @@ class hmp:
                                          [self.mean_d]]) #Durations
         params = np.zeros((n_events+1,2), dtype=np.float64)
         params[:,0] = self.shape
+        averagepos -= .5
         params[:,1] = np.diff(averagepos, prepend=0)
-        params[0,1] -= .5
-        params[1:,1] += .5
         params[:,1] = [self.mean_to_scale(x[1],x[0]) for x in params]
         return params
 
