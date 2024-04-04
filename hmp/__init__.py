7f6184b5035c926c2dd5d01870e77d4ac48baa0a
jelmerborst <jelmerborst@gmail.com>
2024-04-04 16:33:24 +0200

    simulations were not indexed

--- hmp/__init__.py
+++ hmp/__init__.py
@@ -2,10 +2,12 @@
 
 
 from . import models
+from . import simulations
 from . import utils
 from . import visu
 from . import resample
 from . import clusters
+from . import mcca
 from . import loocv
 
-__all__ = ["models", "simulations", "utils","visu", "clusters","mcca","loocv"]
+__all__ = ["models", "simulations", "utils", "visu", "resample", "clusters","mcca","loocv"]
