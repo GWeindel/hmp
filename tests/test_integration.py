265fcf38fb3f3250b78bb0cb648dc192417ad74a
gweindel <gabriel.weindel@gmail.com>
2024-12-13 19:21:43 +0100

    added new tests to increase coverage of visu

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -9,6 +9,7 @@ import xarray as xr
 from mne.io import read_info
 from scipy.stats import gamma
 from pathlib import Path
+import gc
 
 import hmp
 from hmp import simulations
@@ -41,10 +42,10 @@ def test_integration():
     resp_trigger = int(np.max(np.unique(events[:,2])))#Resp trigger is the last source in each trial
     event_id = {'stimulus':1}#trigger 1 = stimulus
     resp_id = {'response':resp_trigger}
-    eeg_data = hmp.utils.read_mne_data(raw, event_id=event_id, resp_id=resp_id, sfreq=sfreq, 
+    epoch_data = hmp.utils.read_mne_data(raw, event_id=event_id, resp_id=resp_id, sfreq=sfreq, 
                 events_provided=events, verbose=False)
-    hmp_data = hmp.utils.transform_data(eeg_data, apply_standard=False, n_comp=2)
-    init = hmp.models.hmp(data=hmp_data, epoch_data=eeg_data, sfreq=eeg_data.sfreq,
+    hmp_data = hmp.utils.transform_data(epoch_data, apply_standard=False, n_comp=2)
+    init = hmp.models.hmp(data=hmp_data, epoch_data=epoch_data, sfreq=epoch_data.sfreq,
                         event_width=50, distribution='gamma', shape=2)
     sim_source_times, true_pars, true_magnitudes, _ = simulations.simulated_times_and_parameters(events, init)
 
@@ -54,8 +55,16 @@ def test_integration():
                             return_max=False,verbose=False)#funct
     hmp.utils.save(selected, 'selected.nc')
     hmp.utils.load('selected.nc')
-    hmp.visu.plot_topo_timecourse(eeg_data, estimates, positions, init, magnify=1, sensors=True, times_to_display = np.mean(np.cumsum(sim_source_times,axis=1),axis=0),ax=ax)
-    assert np.abs(np.sum(init.compute_times(eeg_data,true_estimates) - init.compute_times(eeg_data,estimates)))<1.1 #If error is reasonable
+
+### testing plot_topo
+    
+    hmp.visu.plot_topo_timecourse(epoch_data, estimates, positions, init, magnify=1, sensors=True, times_to_display = np.mean(np.cumsum(sim_source_times,axis=1),axis=0),ax=ax, topo_size_scaling=True, max_time=2)
+    hmp.visu.save_model_topos(epoch_data, estimates, positions, init, fname='topo', figsize=None, dpi=300, cmap='Spectral_r', 
+                vmin=None, vmax=None, sensors=False, contours=6, colorbar=True)
+    p = hmp.visu.plot_topo_timecourse(epoch_data, estimates, positions, init, sensors=False, 
+                                      times_to_display = None, title='a', max_time=None)
+    plt.close()
+    assert np.abs(np.sum(init.compute_times(epoch_data,true_estimates) - init.compute_times(epoch_data,estimates)))<1.1 #If error is reasonable
     #fig, ax = plt.subplots(1)#captures plots
     #hmp.visu.plot_distribution(estimates.eventprobs.sel(trial_x_participant=('S0',1)), 
                                 #xlims=(0,np.percentile(sim_source_times.sum(axis=1), q=90)), ax=ax);
@@ -69,8 +78,8 @@ def test_integration():
         BRP_times = init.compute_times(init, fakefit, fill_value=0, add_rt=True)
         times = BRP_times.sel(event=[0,3])#Stim and response only
         times['event'] = [0,1]
-        erp_data = hmp.visu.erp_data(eeg_data.stack(trial_x_participant=["participant","epochs"]), times, channel)
-        hmp.visu.plot_erp(times, erp_data, c, ax[0], upsample=2, label=channel)
+        erp_data = hmp.visu.erp_data(epoch_data.stack(trial_x_participant=["participant","epochs"]), times, channel)
+        hmp.visu.plot_erp(times, erp_data, c, ax[0], upsample=2, label=channel, bootstrap=2,minmax_lines=[1,2])
     ev_colors = iter(['red', 'purple','brown','black',])
     sim_event_times_cs = np.cumsum(sim_source_times, axis=1)
     for event in range(2):
@@ -85,7 +94,7 @@ def test_integration():
     fig, ax = plt.subplots(1,2, figsize=(9,2), sharey=True, sharex=True)
     colors = iter([plt.cm.tab10(i) for i in range(10)])
 
-    data = eeg_data.stack({'trial_x_participant':['participant','epochs']}).data.dropna('trial_x_participant', how="all")
+    data = epoch_data.stack({'trial_x_participant':['participant','epochs']}).data.dropna('trial_x_participant', how="all")
     times = init.compute_times(init, estimates, fill_value=0, add_rt=True)
 
     # Plotting the single trial aligned events
@@ -122,9 +131,28 @@ def test_integration():
     init_speed = hmp.models.hmp(hmp_speed_data, epoch_data, sfreq=epoch_data.sfreq, cpus=2)
     backward_speed = init_speed.backward_estimation(max_fit=estimates_speed, tolerance=1e-1, max_events=2)
     #LOOCV
-    loocv_model_speed = hmp.loocv.loocv(init_speed, hmp_speed_data, backward_speed, print_warning=False, verbose=False)
-    #hmp.visu.plot_loocv(loocv_model_speed, pvals=True, test='sign', indiv=True, mean=True)
+    hmp.visu.plot_components_sensor(hmp_data, info)
+    times = init.compute_times(init, estimates).values
+    hmp.visu.plot_distribution(times, xlims=False, figsize=(8, 3), survival=False)
+    hmp.visu.plot_distribution(times, xlims=[0,1], figsize=(8, 3), survival=True)
+    hmp.visu.plot_latencies(times, init=init, labels=[], 
+    figsize=False, errs=None, kind='point', legend=True, max_time=None, as_time=True)
+    hmp.visu.plot_latencies(times, init=init, labels=[], 
+    figsize=False, errs=None, kind='point', legend=True, max_time=None, as_time=True)
+    hmp.visu.plot_latencies(estimates, init=init, labels=[], 
+    figsize=False, errs='std', kind='bar', legend=False, max_time=None, as_time=False)
+    hmp.visu.plot_latencies(estimates, init=init, labels=[], 
+    figsize=False, errs='std', kind='bar', legend=False, max_time=None, as_time=True)
+    hmp.visu.save_model_topos(epoch_data, backward_speed, info, init, fname='topo', figsize=None, dpi=300, cmap='Spectral_r', 
+                vmin=None, vmax=None, sensors=False, contours=6, colorbar=True)
+    correct_loocv_model = hmp.loocv.loocv_backward(init, hmp_data, max_events=2)
 
+    loocv_model_speed = hmp.loocv.loocv(init_speed, hmp_speed_data, backward_speed, print_warning=False, verbose=False)
+    
+    hmp.visu.plot_loocv(loocv_model_speed, pvals=True, test='t-test', figsize=(16,5), indiv=True,  mean=False, additional_points=None)
+    fig, ax = plt.subplots(2)#captures plots
+    hmp.visu.plot_loocv(loocv_model_speed, pvals=True, test='sign', figsize=(16,5), indiv=True, mean=True, additional_points = (3.5, loocv_model_speed[0]), ax=ax)
+    hmp.visu.plot_loocv(loocv_model_speed, pvals=False, test='sign', figsize=(16,5), indiv=True, mean=True, additional_points = (3.5, loocv_model_speed[0]), ax=ax)
     # the magnitudes map indicates the events. We have two conditions, speed and accuracy, hence two rows. For speed the third event is missing, indicated by the -1.
     mags_map = np.array([[0, -1],
                         [0, 0]])
@@ -144,9 +172,16 @@ def test_integration():
     #fit the model - note that we use the full data again
     
     model_stage_removed = init.fit_single_conds(magnitudes=mags4, parameters=pars4, pars_map=pars_map, mags_map=mags_map, conds=conds,  cpus=1, tolerance=1e-1, verbose=False)
-    hmp.visu.plot_topo_timecourse(epoch_data, model_stage_removed, info, init, magnify=1, sensors=False, time_step=1000/init.sfreq,xlabel='Time (ms)', event_lines=True, colorbar=True, title="Remove one event",ax=ax) 
-    correct_loocv_model = hmp.loocv.loocv_backward(init, hmp_data, max_events=2)
+    hmp.visu.plot_latencies(model_stage_removed, init=init, labels=[], 
+    figsize=False, errs='std', kind='bar', legend=False, max_time=None, as_time=False)
+    hmp.visu.plot_topo_timecourse(epoch_data, model_stage_removed, info, init, magnify=1, sensors=False, as_time=True, xlabel='Time (ms)', event_lines=True, colorbar=True, title="Remove one event",ax=ax,) 
+    hmp.visu.plot_topo_timecourse(epoch_data, model_stage_removed, info, init, magnify=1, sensors=False, as_time=True, xlabel='Time (ms)', event_lines=True, colorbar=True, title="Remove one event",ax=ax, times_to_display=[1,2]) 
+    hmp.visu.plot_topo_timecourse(epoch_data, model_stage_removed, info, init, magnify=1, sensors=False, as_time=True, xlabel='Time (ms)', event_lines=True, colorbar=True, title="Remove one event",ax=ax, times_to_display=[[1,2]]) 
+    hmp.visu.plot_topo_timecourse(epoch_data, model_stage_removed, info, init, magnify=1, sensors=False, as_time=True, xlabel='Time (ms)', event_lines=True, colorbar=True, title="Remove one event",ax=ax, times_to_display=np.array([1,2]))
 
+    hmp.visu.save_model_topos(epoch_data, model_stage_removed, info, init, fname='topo', figsize=None, dpi=300, cmap='Spectral_r', 
+                vmin=None, vmax=None, sensors=False, contours=6, colorbar=True)
+    correct_loocv_model = hmp.loocv.loocv_backward(init, hmp_data, max_events=2)
     hmp.utils.save_eventprobs(selected.eventprobs, 'selected_eventprobs.csv')
     _loocv_combined = hmp.loocv.loocv(init, hmp_data, model_stage_removed, print_warning=False)
 
@@ -155,3 +190,11 @@ def test_integration():
     os.remove("dataset_raw_raw.fif")
     os.remove("selected_eventprobs.csv")
     os.remove("selected.nc")
+    for filename in os.listdir():
+        if filename.endswith('.png'):
+            os.remove(filename)
+
+    #close all plots
+    plt.cla()
+    plt.close()
+    gc.collect()
