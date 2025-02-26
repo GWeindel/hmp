ef60090cfafa670bc57826f3a3c79a7e80c577f6
gweindel <gabriel.weindel@gmail.com>
2025-02-26 12:22:41 +0100

    Separated data creation, stricter tests

--- tests/test_fixed.py
+++ tests/test_fixed.py
@@ -17,16 +17,12 @@ from hmp.trialdata import TrialData
 real_data = os.path.join("tutorials", "sample_data", "eeg", "processed_0022_epo.fif")
 real_metadata = os.path.join("tutorials", "sample_data", "eeg", "0022.csv")
 
-
-def test_fixed():
-
+def create_data(sfreq, n_events):
     # Testing isolated functions in simulations
     assert simulations.simulation_sfreq() == 600.614990234375    
     epoch_data, sim_source_times, info = simulations.demo(1, 1)
     # Data creation/reading
     ## Simulation parameters
-    sfreq = 102
-    n_events = 3
     n_trials = 2
     cpus=1
     times_a = np.array([[50, 50, 150, 50],
@@ -54,7 +50,13 @@ def test_fixed():
         events.append(np.load(event_file))
         raws.append(raw_file)
     sfreq = 100
-    
+    return raws, events, event_id, resp_id
+
+def test_fixed():
+    sfreq = 100
+    n_events = 3
+    raws, events, event_id, resp_id = create_data(sfreq, n_events)
+    event_a = events[0]
     # Data reading
     epoch_data = hmp.utils.read_mne_data(raws, event_id=event_id, resp_id=resp_id, sfreq=sfreq,
             events_provided=events, verbose=True, reference='average', high_pass=1, low_pass=45,
@@ -66,17 +68,18 @@ def test_fixed():
 
         
    # Comparing to simulated data, asserting that results are the one simulated
-    events_a = np.load(event_a)
     data_a = hmp.utils.participant_selection(hmp_data, 'a')
     event_properties = EventProperties.create_expected(sfreq=data_a.sfreq)
     trial_data = TrialData.from_standard_data(data=data_a, template=event_properties.template)
     init_sim = FixedEventModel(event_properties, n_events=n_events, maximization=False,)
-    sim_source_times, true_pars, true_magnitudes, _ = simulations.simulated_times_and_parameters(events_a, init_sim, trial_data)
+    sim_source_times, true_pars, true_magnitudes, _ = \
+        simulations.simulated_times_and_parameters(event_a, init_sim, trial_data)
     true_loglikelihood, true_estimates = init_sim.fit_transform(trial_data, parameters = np.array([true_pars]), magnitudes=np.array([true_magnitudes]),  verbose=True)
     true_topos = hmp.utils.event_topo(epoch_data, true_estimates.squeeze(), mean=True)
     init_sim = FixedEventModel(event_properties, n_events=n_events, maximization=True,)
     likelihood, estimates = init_sim.fit_transform(trial_data, verbose=True)
     test_topos = hmp.utils.event_topo(epoch_data, estimates.squeeze(), mean=True)
     assert (np.array(simulations.classification_true(true_topos,test_topos)) == np.array(([0,1,2],[0,1,2]))).all()
-    assert np.sum(np.abs(true_topos.data - test_topos.data)) < 2.65e-05
-    assert np.round(likelihood,4) > np.array(-1)
+    print(np.sum(np.abs(true_topos.data - test_topos.data)))
+    assert np.round(np.sum(np.abs(true_topos.data - test_topos.data)), 4) == 0
+    assert np.round(likelihood,4) == np.array(2.4834)
