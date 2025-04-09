facc7ae3e3393d70890f18a917d56516e22ef11d
GWeindel <gabriel.weindel@proton.me>
2025-04-09 11:16:55 +0200

    Upstream function changes for previous commit

--- hmp/models/backward.py
+++ hmp/models/backward.py
@@ -19,13 +19,9 @@ default_colors = ["cornflowerblue", "indianred", "orange", "darkblue", "darkgree
 
 
 class BackwardEstimationModel(BaseModel):
-    def __init__(self, *args, max_events=None, min_events=0, max_starting_points=1,
-                 tolerance=1e-4, max_iteration=1e3, **kwargs):
+    def __init__(self, *args, max_events=None, min_events=0,  **kwargs):
         self.max_events = max_events
         self.min_events = min_events
-        self.max_starting_points = max_starting_points
-        self.tolerance = tolerance
-        self.max_iteration = max_iteration
         self.submodels = {}
         super().__init__(*args, **kwargs)
 
@@ -36,6 +32,8 @@ class BackwardEstimationModel(BaseModel):
         min_events=0,
         base_fit=None,
         cpus=1,
+        tolerance=1e-4, 
+        max_iteration=1e3,
     ):
         """Perform the backward estimation.
 
@@ -57,8 +55,6 @@ class BackwardEstimationModel(BaseModel):
             Tolerance applied to the expectation maximization in the EM() function
         maximization: bool
             If True (Default) perform the maximization phase in EM() otherwhise skip
-        max_iteration: int
-            Maximum number of iteration for the expectation maximization in the EM() function
         """
         if max_events is None and base_fit is None:
             max_events = self.compute_max_events(trial_data)
@@ -66,8 +62,10 @@ class BackwardEstimationModel(BaseModel):
             print(
                 f"Estimating all solutions for maximal number of events ({max_events})"
             )
-            fixed_n_model = self.get_fixed_model(n_events=max_events, starting_points=1)
+            fixed_n_model = self.get_fixed_model(n_events=max_events)
             loglikelihood, eventprobs = fixed_n_model.fit_transform(trial_data, verbose=False,
+                                                                    tolerance=tolerance,
+                                                                    max_iteration=max_iteration,
                                                                     cpus=cpus)
         else:
             loglikelihood, eventprobs = base_fit
@@ -75,7 +73,7 @@ class BackwardEstimationModel(BaseModel):
         self.submodels[max_events] = fixed_n_model
 
         for n_events in np.arange(max_events - 1, min_events, -1):
-            fixed_n_model = self.get_fixed_model(n_events, starting_points=n_events+1)
+            fixed_n_model = self.get_fixed_model(n_events)
 
             print(f"Estimating all solutions for {n_events} events")
 
@@ -98,6 +96,9 @@ class BackwardEstimationModel(BaseModel):
                             magnitudes=np.array(events_temp),
                             parameters=np.array(pars_temp),
                             verbose=False,
+                            tolerance=tolerance,
+                            max_iteration=max_iteration,
+                            starting_points = n_events+1,
                             cpus=cpus
                         )
 
@@ -134,9 +135,6 @@ class BackwardEstimationModel(BaseModel):
             return self._concatted_attr(attr)
         return super().__getattribute__(attr)
 
-    def get_fixed_model(self, n_events, starting_points):
+    def get_fixed_model(self, n_events):
         return FixedEventModel(
-            self.events, self.distribution, n_events=n_events,
-            starting_points=starting_points,
-            tolerance=self.tolerance,
-            max_iteration=self.max_iteration)
+            self.events, self.distribution, n_events=n_events)
