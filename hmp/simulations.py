9962262effd3c3e919ee9af70700f1b3f1501206
GWeindel <gabriel.weindel@proton.me>
2025-05-14 19:10:45 +0200

    Added defaults to distribution class

--- hmp/simulations.py
+++ hmp/simulations.py
@@ -347,7 +347,7 @@ def demo(cpus, n_events, seed=123):
     ## Imports and code specific to the simulation (see tutorial 3 and 4 for real data)
     from scipy.stats import gamma
 
-    from hmp.utils import read_mne_data
+    from hmp.io import read_mne_data
 
     random_gen = np.random.default_rng(seed=seed)
 
