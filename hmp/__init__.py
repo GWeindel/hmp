e6c3e4e6aeb300bbd2cfc5ebb2d7210a287e3706
GWeindel <gabriel.weindel@proton.me>
2025-06-05 07:46:24 +0200

    added preprocessing to __init__ to allow calling it directly

--- hmp/__init__.py
+++ hmp/__init__.py
@@ -1,5 +1,5 @@
 """Software for fitting HMP on EEG/MEG data."""
 
-from . import loocv, models, utils, visu, io
+from . import loocv, models, utils, visu, io, preprocessing
 
-__all__ = ["models", "simulations", "utils", "visu", "io", "mcca", "loocv"]
+__all__ = ["models", "simulations", "utils", "visu", "io", "preprocessing:, "mcca", "loocv"]
