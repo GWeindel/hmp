664ce21835f531c1e50d33b4071420314a3038d7
GWeindel <gabriel.weindel@proton.me>
2025-06-05 08:17:47 +0200

    added distributions and patterns to init

--- hmp/__init__.py
+++ hmp/__init__.py
@@ -1,5 +1,5 @@
 """Software for fitting HMP on EEG/MEG data."""
 
-from . import loocv, models, utils, visu, io, preprocessing
+from . import loocv, models, utils, visu, io, preprocessing, patterns, distributions
 
-__all__ = ["models", "simulations", "utils", "visu", "io", "preprocessing", "mcca", "loocv"]
+__all__ = ["models", "simulations", "utils", "visu", "io", "preprocessing", "patterns", "distributions" ,"mcca", "loocv"]
