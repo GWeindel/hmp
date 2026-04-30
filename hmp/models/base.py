4e8ea0bfdc8440023ca517720efbad5393cc7c45
GWeindel <gabriel.weindel@proton.me>
2026-04-30 18:51:43 +0200

    Fixed computation of max events given new censoring handling

--- hmp/models/base.py
+++ hmp/models/base.py
@@ -50,7 +50,7 @@ class BaseModel(ABC):
 
     def compute_max_events(self, trial_data: TrialData):
         """Compute the maximum possible number of events given location and minimum duration."""
-        return int(np.rint(np.percentile(trial_data.durations, 10) // (self.location)))
+        return int(np.rint(np.min(trial_data.durations) // (self.location)))
 
 
     def __getattribute__(self, attr):
