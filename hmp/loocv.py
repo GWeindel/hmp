979f637fe35a53318524e1d10a955c9be7bc8f96
jelmerborst <jelmerborst@gmail.com>
2026-06-08 17:22:01 +0200

    function

--- hmp/loocv.py
+++ hmp/loocv.py
@@ -22,11 +22,34 @@ import copy
 class LOOCV():
     """LOOCV class. Can be initialized with a (fitted) model or a self-defined function.
 
+    Performs leave-one-out cross validation using either the provided HMP model or
+    user-defined function to calculate the initial fit. It will perform loocv by 
+    leaving out one participant, applying the provided model or function to the
+    data to estimate a fit, and computing the likelihood of the data from the left out
+    participant with the estimated parameters. This is repeated for all participants.
+
+    If a self-defined function is used, it must accept data (see fit(..) below) as its
+    first argument. Additional arguments can be provided through named func_kwargs. 
+    The function is required to return a (list of) fitted model(s); for all provided
+    models the likelihood of the left out participant will be calculated.
+
+    The reason for using a function instead of a model is if a sequence of operations
+    needs to be performed for each subject (e.g., fitting multiple models in sequence,
+    see example_complex_func(..) below.
+
+    For an example of potential functions, see at the bottom of this file:
+    example_simple_func(..)
+    example_complex_func(..)
+
     Parameters
     ----------
     model : Any 
-        either a (fitted) model or a self-defined function to be applied to provided data
-    function_args :
+        either a (fitted) HMP model or a self-defined function to be applied to
+        provided data. If function, must accept data as its first argument and 
+        return a (list of) fitted model(s). See explanation above.
+    function_kwargs : dict
+        additional arguments to pass on to self-defined function.
+        Default = None.
     quick : bool, optional
         Toggle for quick LOOCV using parameters of fitted model. 
         Typically incorrect, see info message. Requires fitted model.
@@ -38,7 +61,6 @@ class LOOCV():
         when providing a function as model in combination with preprocessed data.
         If model or PatternData is provided, Pattern in model or PatternData takes 
         precedence in fit.
-        pattern is required when PCA_cv = True in combination with function. If not, defaults to halfsine 50 ms.
     verbose: bool, optional
         Default = True
     print_warning : bool, optional
@@ -48,6 +70,7 @@ class LOOCV():
     def __init__(
         self,
         model: Any,
+        function_kwargs: dict = None,
         quick: bool = False,
         pattern: hmp.patterns.Pattern = None,
         verbose: bool = True,
@@ -55,6 +78,7 @@ class LOOCV():
     ):
 
         self.model = model
+        self.function_kwargs = function_kwargs
         self.quick = quick
 
         #instance of an hmp model
@@ -102,7 +126,6 @@ class LOOCV():
             if self.model_class != "function":
                 self.pattern = self.model.pattern
             else:
-                #need assert pca here, then pattern required maybe warn and make default halfsine
                 self.pattern = None
         else: #pattern provided
             if self.model_class != "function":
@@ -202,9 +225,9 @@ class LOOCV():
                         self.participants_idx,
                         itertools.repeat(1), #cpus_model has to be 1
                         itertools.repeat(verbose)
-                    ),
+                    )
                 )
-
+        
         # if multiple estimates are returned per subject, rearrange data so
         # each item of all_estimates will contain one estimate for each subject.
         # So for eliminative, all_estimates[0] will have all n-stage subject
@@ -256,9 +279,10 @@ class LOOCV():
                             estimates,
                             itertools.repeat(1), #cpus
                             itertools.repeat(verbose),
-                        ),
+                        )
                     )
 
+            #format results
             likelihoods = xr.DataArray(
                 np.expand_dims(np.array(loocv).astype(np.float64), axis=0),
                 dims=("n_event", "participant"),
@@ -279,9 +303,7 @@ class LOOCV():
             
             all_likelihoods.append(likelihoods)
 
-
-
-        #In case of eliminative, we can concat likelihoods
+        #In case of eliminative, we can concat likelihoods and estimates
         if self.model_class == hmp.models.EliminativeMethod:
             all_likelihoods = xr.concat(all_likelihoods, dim="n_event")
             all_likelihoods.attrs['n_event'] = np.zeros(all_likelihoods.data.shape).astype(int)
@@ -332,7 +354,7 @@ class LOOCV():
 
         # Fit model on data
         if self.model_class == "function":
-            print("don't know what to do yet!")
+            estimated_model = self.model(data, **self.function_kwargs)
         else:
             if not self.quick: #same for Event and Eliminative
                 estimated_model = copy.deepcopy(self.model)
@@ -342,7 +364,8 @@ class LOOCV():
                     estimated_model = copy.deepcopy(self.model)
                     estimated_model.fit(data=data, channel_pars=estimated_model.channel_pars,\
                          time_pars=estimated_model.time_pars, cpus=cpus_model, verbose= False)
-                elif self.model_class == hmp.models.EliminativeMethod: #use prev params per model
+                elif self.model_class == hmp.models.EliminativeMethod: 
+                #use prev params per model
                     estimated_model = []
                     for n_events in range(1, self.model.max_events + 1):
                         estimated_model.append(copy.deepcopy(self.model.submodels[n_events]))
@@ -353,22 +376,8 @@ class LOOCV():
 
         return estimated_model
 
-    #estimates = []
-    #simple EventModel - might be possible for all
-    #if self.model_class == hmp.models.EventModel:
-    #    pass
 
-    # Apply function and return
-    #estimates = func_estimate(model_without_pp, *func_args)
-    #if isinstance(estimates, list):
-    #    for i in range(len(estimates)):
-    #        estimates[i] = estimates[i].drop_vars(["eventprobs"])
-    #else:
-    #    estimates = estimates.drop_vars(["eventprobs"])
-
-    #return estimates
-
-    def loocv_loglikelihood(self, data,participant, estimate, cpus_model=1, verbose=False):
+    def loocv_loglikelihood(self, data, participant, estimate, cpus_model=1, verbose=False):
         """Compute the log-likelihood of the fit.
 
         Calculate loglikelihood of fit on participant using parameters from estimate,
@@ -471,4 +480,69 @@ class LOOCV():
 
 
 
-## add function to return average mdoel
+## add function to return average model.
+# follow structures of estimates
+# if variable nr of events, return average for each number
+
+
+
+
+### Example loocv functions
+def example_simple_func(data, n_events, magnitudes=None, parameters=None, verbose=False):
+    """Example of a simple function that estimated an n_event model.
+
+    Note that this would normally not be done, as you could just provide
+    this model directly to loocv(..). This is only an example of how a 
+    function could be used, see below for a more realistic example.
+
+    Examples
+    --------
+    loocv_simple_func = hmp.loocv.LOOCV(example_simple_func, func_args={'n_events' : 2})
+    lkh_simple_func, estimates_simple_func = loocv_simple_func.fit(data)
+    """
+
+    ev_model = hmp.models.EventModel(n_events=n_events)
+    return ev_model.fit(data, magnitudes=magnitudes, parameters=parameters, verbose=verbose)
+
+
+def example_complex_func(
+    hmp_model, max_events=None, n_events=1, mags_map=None, pars_map=None, conds=None, verbose=False
+):
+    """Fit function, complex example.
+
+    Example of a complex function that can be used with loocv_func.
+    This function first performs backwards estimation up to max_events,
+    and follows this with a condition-based model of n_events, informed
+    by the selected backward model and the provided maps. It returns
+    both models, so for both the likelihood will be estimated.
+
+    Examples
+    --------
+    >>> pars_map = np.array([[0, 0, 0, 0, 0, 0],
+    >>>                      [0, 0, 0, 0, 1, 0],
+    >>>                      [0, 0, 0, 0, 2, 0],
+    >>>                      [0, 0, 0, 0, 3, 0],
+    >>>                      [0, 0, 0, 0, 4, 0]])
+    >>> conds = {'rep': np.arange(5)+1}
+    >>> loocv_func(hmp_model, hmp_data, example_complex_fit_n_func,
+    >>>            func_args=[7, 5, None, pars_map,conds])
+    """
+    # fit backward model up to max_events
+    backward_model = hmp_model.backward_estimation(max_events)
+
+    # select n_events model
+    n_event_model = backward_model.sel(n_events=n_events).dropna("event", how="all")
+    mags = n_event_model.magnitudes.dropna("event", how="all").data
+    pars = n_event_model.parameters.dropna("stage").data
+
+    # fit condition model
+    cond_model = hmp_model.fit_n(
+        magnitudes=mags,
+        parameters=pars,
+        mags_map=mags_map,
+        pars_map=pars_map,
+        level_dict=conds,
+        verbose=verbose,
+    )
+
+    return [backward_model, cond_model]
