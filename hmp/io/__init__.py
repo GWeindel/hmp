1db383e490fc3e09d038a9d116eca9f13f8cab0f
GWeindel <gabriel.weindel@proton.me>
2026-06-30 14:16:23 +0200

    added add_metadata function

--- hmp/io/__init__.py
+++ hmp/io/__init__.py
@@ -3,5 +3,6 @@
 from hmp.io.bids_raw import read_bids_raw
 from hmp.io.mne_raw import read_mne_raw
 from hmp.io.mne_epochs import read_mne_epochs
+from hmp.io.metadata import add_metadata
 
-__all__ = ["read_bids_raw", "read_mne_raw", "read_mne_epochs"]
+__all__ = ["read_bids_raw", "read_mne_raw", "read_mne_epochs", "add_metadata"]
