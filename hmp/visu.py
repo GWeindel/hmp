f3d935ed4c0fda325a1a7ff35dbf4e60ee472ba2
jelmerborst <jelmerborst@gmail.com>
2026-07-18 18:41:12 +0200

    linting

--- hmp/visu.py
+++ hmp/visu.py
@@ -258,7 +258,8 @@ def plot_topo_timecourse(  # noqa  # Might need some serious refactoring.
         topo_size = event_size * magnify
     else:
         timescale = (
-            max_time if max_time else (np.nanmax((np.nanmax(times_to_display), np.nanmax(times[:, -1]))) * 1.05)
+            max_time if max_time else (np.nanmax((np.nanmax(times_to_display), \
+                np.nanmax(times[:, -1]))) * 1.05)
         ) + time_step
         topo_size = 0.08 * timescale * magnify  # 8% of time scale
 
